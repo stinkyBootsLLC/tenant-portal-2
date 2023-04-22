526a9baf3c88640e3ec1db5517d86193cad01e5f
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-04-21 20:07:59 -0400

    ts

--- app/tenants/includes/tenantFunctions.php
+++ app/tenants/includes/tenantFunctions.php
@@ -25,6 +25,8 @@
             $stmt->bindValue(':userPassword', $hashPassword, SQLITE3_TEXT);
             $result = $stmt->execute();
             $row = $result->fetchArray(MYSQLI_ASSOC);
+
+            var_dump($row);
        
             // the very first ID is 1 so 0 does not exist
             if ($row["Tenant_ID"] > 0) {  
